9a06209a0575d80dcf7be53e4479533e400d932d
Remyson Costa <remysonrodrigues@gmail.com>
2020-07-07 21:01:46 -0300

    add Scenes Fases

---
+++ Assets/Scripts/moveOffset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveOffset : MonoBehaviour
+{
+    private Material materialAtual;
+    public float velocidadeX;
+    private float escalaMovimento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        materialAtual = GetComponent<Renderer>().material; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        escalaMovimento += 0.01f;   
+
+        materialAtual.SetTextureOffset("_MainTex", new Vector2(escalaMovimento * velocidadeX, 0));
+    }
+}
