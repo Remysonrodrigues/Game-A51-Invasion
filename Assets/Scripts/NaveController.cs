5c01bc9fde3daa08825f7c2e7e7b15c1015cd17b
Remyson Costa <remysonrodrigues@gmail.com>
2020-07-13 22:06:07 -0300

    add script bulletController

--- Assets/Scripts/NaveController.cs
+++ Assets/Scripts/NaveController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class NaveController : MonoBehaviour
 {
+    public GameObject projetil;
+    public int life = 5;
+    public int maxProjetil;
+    public int projetils;
+    public bool podeRecarregar = true;    
+
     private Rigidbody2D naveRb;
     public float velociadeMove;
     
@@ -11,16 +17,41 @@ public class NaveController : MonoBehaviour
     void Start()
     {
         naveRb = GetComponent<Rigidbody2D>();
+        projetils = maxProjetil;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Movimentação
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         naveRb.velocity = new Vector2(
             horizontal * velociadeMove,
             vertical * velociadeMove
         );
+        // Disparos
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if ( projetils > 0)
+            {
+                Instantiate(projetil, transform.position, transform.rotation);
+                projetils -= 1;
+            }            
+        }
+        // Recarga
+        if(projetils < maxProjetil && podeRecarregar == true)
+        {
+            podeRecarregar = false;
+            StartCoroutine(Recarregar());
+        }
     }
+
+    IEnumerator Recarregar()
+    {
+        yield return new WaitForSeconds(0.5f);
+        projetils += 1; 
+        podeRecarregar = true;
+    }
+
 }
---
+++ Assets/Scripts/projetilController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projetilController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(HoraDeDestruir());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, -0.1f, 0);
+    }
+
+    private void OnCollisionEnter(Collision coli)
+    {
+        if (coli.gameObject.tag != "Player")
+        {
+            Destroy(coli.gameObject);
+            Destroy(gameObject);                    
+        }
+    }
+
+    IEnumerator HoraDeDestruir()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+
+}
