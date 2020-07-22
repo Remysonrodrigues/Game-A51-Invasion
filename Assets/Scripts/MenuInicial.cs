c40463c23c23fff9be27c8b829e351da68631814
BrigidoC <brigidoconrado15@gmail.com>
2020-07-21 21:50:05 -0300

    Menu e Cutscene

---
+++ Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    int primeiraVez = 0;
+    int secunds= 36;
+    public void ChamaCutscene()
+    {   
+        if(primeiraVez == 0){
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Cutscene");
+            primeiraVez++;
+        }
+        else{
+             UnityEngine.SceneManagement.SceneManager.LoadScene("SceneFase1");
+        }
+    }
+    public void menu()
+    {
+
+    }
+    public void SairDoJogo()
+    {
+        Application.Quit();
+    }
+
+      void ChamaFase1()
+    {
+         if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("SceneFase1");     
+        }
+    }
+
+}
