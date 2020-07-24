13a337c972964b46102b9248db09faf034dc8fb9
Rarysson Guilherme <r_guilherme12@hotmail.com>
2020-07-24 01:07:56 -0300

    Mudado os disparos dos inimigos e adiocionado lança misseis

---
+++ Assets/Scripts/MissilController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissilController : MonoBehaviour
+{
+    public GameObject projetil;
+    public int life = 10;
+    bool naveEmCima = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Disparar() {
+        GameObject nave = GameObject.Find("Nave");
+
+        if(nave != null && naveEmCima)
+        {
+            GameObject disparo = (GameObject) Instantiate(projetil);
+
+            disparo.transform.position = transform.position;
+            disparo.transform.Translate(0, 1, 0);
+            Vector3 direction = nave.transform.position - disparo.transform.position;
+            disparo.GetComponent<projetilInimigoController>().SetDirection(direction);
+
+            yield return new WaitForSeconds(1);
+
+            StartCoroutine(Disparar());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.name == "Nave") {
+            naveEmCima = true;
+            StartCoroutine(Disparar());
+        }
+    }
+
+    IEnumerator OnTriggerExit2D(Collider2D col)
+    {
+        if (col.name == "Nave") {
+            Disparar();
+            yield return new WaitForSeconds(1);
+            naveEmCima = false;
+        }
+    }
+}
--- Assets/Scripts/inimigoController.cs
+++ Assets/Scripts/inimigoController.cs
@@ -6,16 +6,20 @@ public class inimigoController : MonoBehaviour
 {
     public GameObject projetil;
     //public GameObject nave;
-    public int life = 5; 
+    public int life = 5;
 
-    private Rigidbody2D inimigoRb;
+    // private Rigidbody2D inimigoRb;
     public float velociadeMove;
-    
+    Animator animator;
+    bool naveEmCima = false;
+    bool atirando = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        inimigoRb = GetComponent<Rigidbody2D>();
-        StartCoroutine(Disparar());
+        // inimigoRb = GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+        // StartCoroutine(Disparar());
     }
 
     // Update is called once per frame
@@ -44,23 +48,64 @@ public class inimigoController : MonoBehaviour
         */
     }
 
+    // IEnumerator Disparar()
+    // {
+    //     GameObject nave = GameObject.Find("Nave");
+
+    //     if(nave != null)
+    //     {
+    //         GameObject disparo = (GameObject) Instantiate(projetil);
+
+    //         disparo.transform.position = transform.position;
+
+    //         Vector3 direction = nave.transform.position - disparo.transform.position;
+
+    //         disparo.GetComponent<projetilInimigoController>().SetDirection(direction);
+
+    //         yield return new WaitForSeconds(1);
+
+    //         StartCoroutine(Disparar());
+    //     }
+    // }
     IEnumerator Disparar()
     {
-        GameObject nave = GameObject.Find("Nave");
+        if (naveEmCima && !atirando) {
+            float count = 0f;
+            atirando = true;
 
-        if(nave != null)
-        {
-            GameObject disparo = (GameObject) Instantiate(projetil);
+            for (int i = 0; i < 5; i++) {
+                GameObject disparo = (GameObject) Instantiate(projetil);
 
-            disparo.transform.position = transform.position;
+                Vector3 direction = Vector3.up;
+                disparo.transform.position = transform.position;
+                disparo.transform.Translate(-1 + count, 1, 0);
+                disparo.GetComponent<projetilInimigoController>().SetDirection(direction);
+                count += 0.25f;
+                yield return new WaitForSeconds(0.2f);
+            }
 
-            Vector3 direction = nave.transform.position - disparo.transform.position;
+            atirando = false;
 
-            disparo.GetComponent<projetilInimigoController>().SetDirection(direction);
-
-            yield return new WaitForSeconds(1);
-            
             StartCoroutine(Disparar());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.name == "Nave") {
+            animator.Play("Atacando");
+            naveEmCima = true;
+            StartCoroutine(Disparar());
+        }
+    }
+
+    IEnumerator OnTriggerExit2D(Collider2D col)
+    {
+        if (col.name == "Nave") {
+            Disparar();
+            yield return new WaitForSeconds(1);
+            naveEmCima = false;
+            animator.Play("parado");
         }
     }
 }
