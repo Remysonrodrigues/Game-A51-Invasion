278ac37ead1a6c058691795833f207428a809314
Rarysson Guilherme <r_guilherme12@hotmail.com>
2020-07-26 00:09:15 -0300

    Adicionado fundo parallax que se move de acordo com o jogador

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public FreeParallax parallax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movimentoHorizontal = Input.GetAxis("Horizontal");
+
+        if (parallax != null)
+        {
+            if (movimentoHorizontal < 0) // Indo para Esquerda
+            {
+                parallax.Speed = 10.0f;
+            }
+            else if (movimentoHorizontal > 0) // Indo para Direita
+            {
+                parallax.Speed = -10.0f;
+            }
+            else
+            {
+                parallax.Speed = 0.0f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public GameObject player;
+
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start ()
+    {
+        offset = transform.position - player.transform.position;
+        offset = new Vector3(offset.x, 0, 0);
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate ()
+    {
+        if (player != null) {
+            Vector3 tmp = player.transform.position + offset;
+            transform.position = new Vector3(tmp.x, 0, -10);
+        }
+    }
+}
